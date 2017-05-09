a6c7665b1e2ebc29b8599598253f5c055eda87ff
Alen Thomas <alenthomas9@gmail.com>
2017-05-09 16:04:21 +0530

    adds a simple number parser

--- j_parser.py
+++ j_parser.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+import re
 
 def string_parser(string):
     str_list = []
@@ -11,6 +12,7 @@ def string_parser(string):
         str_list.append(s)
         str_list.append(string[i+1:])
         return str_list
+
 def colon_parser(string):
     str_list = []
     if string[0] == ':':
@@ -18,8 +20,22 @@ def colon_parser(string):
         str_list.append(string[1:])
         return str_list
 
+def number_parser(string):
+    exp = r'^[0-9]+'
+    pattern = re.compile(exp)
+    str_list = []
+    if pattern.match(string[0]):
+        s = ''
+        i = 0
+        while string[i] is not (',' or '}' or ']'):
+            s = s + string[i]
+            i = i+1
+        str_list.append(s)
+        str_list.append(string[i+1:])
+        return str_list
+
 def jparser(string):
-    val = colon_parser(string)
+    val = number_parser(string)
     if val:
         return val
     else:
